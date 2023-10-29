c52b06ca012f2150d40d4d43ba15b719ab5b43a5
Siddharth Nand <sidnand@student.ubc.ca>
2023-10-29 13:44:37 -0700

    connected to oracle

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <title>CPSC 304 Project</title>
 </head>
 <body>
+    <?php
+        if ($c=OCILogon("ora_sidnand", "a76648070", "dbhost.students.cs.ubc.ca:1522/stu")) {
+            echo "Successfully connected to Oracle.\n";
+            OCILogoff($c);
+        } else {
+            $err = OCIError();
+            echo "Oracle Connect Error " . $err['message'];
+        }
+    ?>
+
+
     <p>Hello, world!</p>
 </body>
 </html>
