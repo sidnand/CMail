9b8fe2fcd733b757633bfbae98d3a8a64a1ced81
sfkrnwi <sfkrnwi@students.cs.ubc.ca>
2023-11-07 22:46:10 -0800

    added functionality to insert an account

--- user.php
+++ user.php
@@ -25,6 +25,71 @@
             exit;
         }
     }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $conn = connect_to_oracle();
+        $username = $_POST["username"];
+        $account = get_account($conn, $username);
+
+        if ($account) {
+            $error_message = "Username already exists. Please choose another.";
+            oci_close($conn);
+        } else {
+            $result = insert_account($conn, $username);
+            if (!$result) {
+                $error_message = "Error creating user.";
+                oci_close($conn);
+            } else {
+                header('Location: account.php');
+            }
+        }
+    }
+
+    function insert_account($conn, $username) {
+        $adminsNumber = rand(1010, 1014);
+        $ownersPhoneNumber = $_SESSION['phoneNumber'];
+        $currentDate = new DateTime();
+        $formattedDate = $currentDate->format("Y-m-d");
+
+        $query = "INSERT INTO Account VALUES (:username, TO_DATE(:currentDate, 'YYYY-MM-DD'), :ownersPhoneNumber, :adminsNumber)";
+        $stmt = oci_parse($conn, $query);
+        oci_bind_by_name($stmt, ":username", $username);
+        oci_bind_by_name($stmt, ":currentDate", $formattedDate);
+        oci_bind_by_name($stmt, ":ownersPhoneNumber", $ownersPhoneNumber);
+        oci_bind_by_name($stmt, ":adminsNumber", $adminsNumber);
+
+        $result = oci_execute($stmt);
+        if (!$result) {
+            $error_message = "Oracle Execute Error " . oci_error($stmt)['message'];
+            echo "Error message: " . $error_message;
+            oci_close($conn);
+            return false;
+        }
+        return true;
+    }
+    
+
+    function get_account($conn, $username) {
+        $query = "SELECT * FROM Account WHERE username = :username";
+        $stmt = oci_parse($conn, $query);
+        oci_bind_by_name($stmt, ":username", $username);
+    
+        $result = oci_execute($stmt);
+        if (!$result) {
+            oci_close($conn);
+            return false;
+        }
+    
+        if (oci_fetch($stmt)) {
+            return array(
+                "username" => oci_result($stmt, "USERNAME"),
+                "dateCreated" => oci_result($stmt, "DATECREATED"),
+                "ownersPhoneNumber" => oci_result($stmt, "OWNERSPHONENUMBER"),
+                "adminsNumber" => oci_result($stmt, "ADMINSNUMBER")
+            );
+        }
+        return false;
+    }                   
 ?>
 
 <div class="container">
@@ -35,7 +100,7 @@
     <?php if (isset($_SESSION['userLoggedIn'])): ?>
     <div class="card mb-3">
         <div class="card-body d-flex flex-column">
-            <h2 class="card-title">Welcome to your account, <?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName']; ?></h2>
+            <h2 class="card-title">Welcome to your home page, <?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName']; ?></h2>
             <div class="d-flex gap-4 pt-3">
                 <form method="post">
                     <button type="button" class="btn btn-primary" name="select" onclick="toggleCard('select')">Select An Account</button>
@@ -58,7 +123,7 @@
     <div id="select" style="display: none;">
         <div class="card mb-3">
             <div class="card-body d-flex flex-column">
-                <h2 class="card-title">Second Card Content</h2>
+                <h2 class="card-title">Select An Account</h2>
                 <p></p>
             </div>
         </div>
@@ -66,15 +131,33 @@
     <div id="add" style="display: none;">
         <div class="card mb-3">
             <div class="card-body d-flex flex-column">
-                <h2 class="card-title">third Card Content</h2>
-                <p></p>
+                <h2 class="card-title mb-3">Add An Account</h2>
+                <?php
+                    
+                    if ($error_message != "") {
+                        echo "<div class='alert alert-danger' role='alert'>"
+                            . $error_message
+                            . "</div>";
+                    }
+                
+                ?>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                    <div class="row">
+                        <div class="col">
+                            <input type="username" name="username" class="form-control" id="username" placeholder="Username" required>
+                        </div>
+                        <div class="col">
+                            <input type="submit" name="Create Account" class="btn btn-success" value="Create Account">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
     <div id="delete" style="display: none;">
         <div class="card mb-3">
             <div class="card-body d-flex flex-column">
-                <h2 class="card-title">fourth Card Content</h2>
+                <h2 class="card-title">Add An Account</h2>
                 <p></p>
             </div>
         </div>
